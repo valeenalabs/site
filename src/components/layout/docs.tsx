64c19ccd21d9f32dcbfe6f8edc084dda271f74db
Baran Mordoğan <baran@okunamayanad.com>
2025-12-07 23:12:50 +0300

    fix: update dependencies and fix breaking changes to patch RCE
    https://www.cve.org/CVERecord?id=CVE-2025-55182

--- src/components/layout/docs.tsx
+++ src/components/layout/docs.tsx
@@ -6,7 +6,7 @@ import { cn } from "../../lib/cn";
 import { TreeContextProvider, useTreeContext } from "fumadocs-ui/contexts/tree";
 import Link from "fumadocs-core/link";
 import { useSearchContext } from "fumadocs-ui/contexts/search";
-import { useSidebar } from "fumadocs-ui/contexts/sidebar";
+import { useSidebar } from "fumadocs-ui/components/sidebar/base";
 import { usePathname } from "fumadocs-core/framework";
 import {
   Search,
